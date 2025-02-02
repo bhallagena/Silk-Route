dad8b6a7e8e4feb23af37b6f08fd7afb8dfaf955
Atmajo <atmajoc@gmail.com>
2025-02-03 02:16:48 +0530

    Add initial card data structure

---
+++ data/index.ts
@@ -0,0 +1,7 @@
+export const carddata = [
+    {
+        name: "Card 1",
+        description: "This is the first card",
+        image: ["https://via.placeholder.com/150", "https://via.placeholder.com/150"],
+    }
+]
